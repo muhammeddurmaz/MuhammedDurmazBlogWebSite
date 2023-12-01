8f0e4ab5c005183893e1f48447ed175d0f9715f2
muhammeddurmaz <mdurmaz.dev@gmail.com>
2023-12-01 16:22:45 +0300

    Add project files.

---
+++ WebUI/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}

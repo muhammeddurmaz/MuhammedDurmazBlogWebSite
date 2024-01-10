0a7d0d4113ffce2652930210ec40104341c2f3b8
muhammeddurmaz <mdurmaz.dev@gmail.com>
2024-01-10 22:34:15 +0300

    blogDtos Added

---
+++ Entity/Dto/Blogs/BlogCreateDTO.cs
@@ -0,0 +1,20 @@
+﻿using Entity.Dto.Categories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Dto.Blogs
+{
+    public class BlogCreateDTO
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public Guid CategoryId { get; set; }
+        public Image Image { get; set; }
+        public IList<CategoryDTO> Categories { get; set; }
+        public string Author { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Entity/Dto/Blogs/BlogDTO.cs
@@ -0,0 +1,22 @@
+﻿using Entity.Dto.Categories;
+using Entity.Dto.Images;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Dto.Blogs
+{
+    public class BlogDTO
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int ViewCount { get; set; }
+        public CategoryDTO Category { get; set; }
+        public bool IsActive { get; set; }
+        public ImageDTO Image { get; set; }
+        public string Author { get; set; }
+    }
+}

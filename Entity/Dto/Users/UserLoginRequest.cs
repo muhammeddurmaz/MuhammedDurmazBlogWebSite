e11e8688876a7bcfa10d7bafa62a2260745f4a2a
muhammeddurmaz <mdurmaz.dev@gmail.com>
2024-01-10 22:36:05 +0300

    .

---
+++ Entity/Dto/Users/UserLoginRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Dto.Users
+{
+    public class UserLoginRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}

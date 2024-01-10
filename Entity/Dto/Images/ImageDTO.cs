2d857ec41cb5733f154210dfba2d4968900c5cd7
muhammeddurmaz <mdurmaz.dev@gmail.com>
2024-01-10 22:35:31 +0300

    ImagesDtos Added

---
+++ Entity/Dto/Images/ImageDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Dto.Images
+{
+    public class ImageDTO
+    {
+        public Guid Id { get; set; }    
+        public string Url { get; set; }
+        public string Title { get; set; }
+    }
+}

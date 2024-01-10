c0e5d930fb2abdd0039dcae3f4768389aae3490e
muhammeddurmaz <mdurmaz.dev@gmail.com>
2024-01-10 22:35:43 +0300

    ProjectDtos Added

---
+++ Entity/Dto/Projects/ProjectCreateRequestDTO.cs
@@ -0,0 +1,20 @@
+﻿using Entity.Dto.Images;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Dto.Projects
+{
+    public class ProjectCreateRequestDTO
+    {
+        public string Url { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ImageCreateRequest Image { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}

78a1846f40f9138caf18624f65a799cc8c1f7ab6
muhammeddurmaz <mdurmaz.dev@gmail.com>
2024-01-10 22:34:41 +0300

    CategoryDtos Added

---
+++ Entity/Dto/Categories/CategoryCreateDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Dto.Categories
+{
+    public class CategoryCreateDTO
+    {
+        public string Name { get; set; }
+    }
+}

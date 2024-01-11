83861b08e4602bffc8a424d57fe78e1f72f1e521
muhammeddurmaz <mdurmaz.dev@gmail.com>
2024-01-11 16:03:41 +0300

    DataLayerExtensions Added

---
+++ Data/Extensions/DataLayerExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Data.Abstract;
+using Data.Concrete;
+using Data.UnitOfWorks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Extensions
+{
+    public static class DataLayerExtensions
+    {
+        public static IServiceCollection LoadDataLayerExtension(this IServiceCollection services,IConfiguration config)
+        {
+            services.AddScoped(typeof(IRepository<>),typeof(EfCoreRepository<>));
+            services.AddScoped<IBlogRepository, EfCoreBlogRepository>();
+			services.AddScoped<IImageRepository, EfCoreImageRepository>();
+			services.AddScoped<ICategoryRepository, EfCoreCategoryRepository>();
+            services.AddScoped<IProjectRepository, EfCoreProjectRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
+
+            services.AddDbContext<WebSiteContext>(opt => opt.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+            return services;
+        }
+    }
+}

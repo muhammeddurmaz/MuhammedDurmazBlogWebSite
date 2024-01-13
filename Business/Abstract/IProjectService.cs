c23a1c603b1efe18c4e50915d75db2f18343adaf
muhammeddurmaz <mdurmaz.dev@gmail.com>
2024-01-13 23:28:59 +0300

    abstract services added

---
+++ Business/Abstract/IProjectService.cs
@@ -0,0 +1,23 @@
+﻿using Entity.Dto.Projects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IProjectService
+    {
+        void Insert(ProjectCreateRequestDTO projectDto);
+        ProjectViewDTO GetProjectViewById(Guid id);
+        List<ProjectViewDTO> GetAllProjectViews();
+        List<ProjectAdminViewDTO> GetAllProjectAdminViews();
+        List<ProjectAdminViewDTO> GetDeletedProjects();
+        void Update(ProjectUpdateDTO projectDTO);
+        void SafeDelete(Guid id);
+        void HardDelete(Guid id);
+        void UndoDelete(Guid id);
+        ProjectDTO GetProjectDTOById(Guid id);
+    }
+}

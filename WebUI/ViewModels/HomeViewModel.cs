bc1280b804b2dc5db6a12c168e6752f08f9ef548
muhammeddurmaz <mdurmaz.dev@gmail.com>
2024-01-19 00:44:21 +0300

    viewModel added

---
+++ WebUI/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Entity.Dto.Blogs;
+using Entity.Dto.Contacts;
+using Entity.Dto.HomePages;
+
+namespace WebUI.ViewModels
+{
+    public class HomeViewModel
+    {
+        public ContactViewDTO ContactViewDTO { get; set; }
+        public HomePageViewDTO HomePageViewDTO { get; set; }
+        public List<BlogDTO> Blogs { get; set; }
+    }
+}

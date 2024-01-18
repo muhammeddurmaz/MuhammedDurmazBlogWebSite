80016beee03f2e30b1d78a022a4089385ba84a64
muhammeddurmaz <mdurmaz.dev@gmail.com>
2024-01-19 00:44:06 +0300

    viewComponents added

---
+++ WebUI/ViewComponents/SkillViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents
+{
+    public class SkillViewComponent : ViewComponent
+    {
+        private readonly ISkillService skillService;
+
+        public SkillViewComponent(ISkillService skillService) 
+        {
+            this.skillService = skillService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var result = skillService.GetAllViews();
+            return View(result);
+        }
+    }
+}
